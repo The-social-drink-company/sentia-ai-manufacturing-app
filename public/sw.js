464712c0f47e7c647c57767738c25f615d5d8755
Dudley Peacock <dudley@financflo.ai>
2025-09-05 11:29:53 +0100

    Dashboard Update Agent: Cycle 22 - 2025-09-05T10:29:53.154Z

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 
-// Dashboard Service Worker - Updated 2025-09-05T10:27:52.997Z
-const CACHE_NAME = 'dashboard-v1757068072997';
+// Dashboard Service Worker - Updated 2025-09-05T10:29:53.018Z
+const CACHE_NAME = 'dashboard-v1757068193018';
 const urlsToCache = [
   '/',
   '/dashboard',
